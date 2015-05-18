876099d83a97c108478d0a4ed121ada0a2fca463
Stella Gavrailova <stella.gavrailova@gmail.com>
2015-05-18 11:41:29 +0300

    fixing some class methods

--- week5/2-Annotations-Reflection/src/task2/JUnitTest.java
+++ week5/2-Annotations-Reflection/src/task2/JUnitTest.java
@@ -9,89 +9,120 @@ import java.util.List;
 import task2.Before.Priority;
 
 public class JUnitTest {
-    
-    @Before (priority = Priority.MEDIUM)
+
+    @Before(priority = Priority.MEDIUM)
     public void funcBefore2() {
         System.out.println("the second before function");
     }
-    
+
     @Before(priority = Priority.HIGHT)
-    public void funcBefore () {
+    public void funcBefore() {
         System.out.println("before function");
     }
-    
+
     @Execute()
     public static void funcExecute() {
         System.out.println("executing function");
     }
-    
+
     @Execute()
     public static void funcExecute2() {
         System.out.println("executing function");
     }
 
-    @After( priority = Priority.MEDIUM) 
+    @After(priority = Priority.MEDIUM)
     public void funcAfter() {
         System.out.println("after function");
     }
-    
-    @After( priority = Priority.LOW) 
+
+    @After(priority = Priority.LOW)
     public void funcAfter2() {
         System.out.println("the second after function");
     }
-    
-    public static void main(String[] args) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-        
-        JUnitTest test = new JUnitTest();
-        
+
+    public static void extractAnnotations(JUnitTest test, List<Method> before, List<Method> execute, List<Method> after) {
+
         Method[] methods = test.getClass().getDeclaredMethods();
-        
-        List< Method> before = new ArrayList<Method>();
-        List< Method> execute = new ArrayList<Method>();
-        List< Method> after = new ArrayList<Method>();
-        
-        for ( Method m : methods) {
+
+        for (Method m : methods) {
             if (m.isAnnotationPresent(Before.class))
                 before.add(m);
             if (m.isAnnotationPresent(Execute.class))
                 execute.add(m);
             if (m.isAnnotationPresent(After.class))
                 after.add(m);
-          }
-        
-     Comparator<Method> c = new Comparator<Method>() {
-
-        @Override
-        public int compare(Method o1, Method o2) {
-            if(o1.getAnnotation(Before.class) != null) {    
-                Before left = (Before) o1.getAnnotation(Before.class);
-                Before right = (Before) o2.getAnnotation(Before.class);
-                 return right.priority().compareTo(left.priority());
-
-            } else if (o1.getAnnotation(After.class) != null) {
-                After left = (After) o1.getAnnotation(After.class);
-                After right = (After) o2.getAnnotation(After.class);
-                return right.priority().compareTo(left.priority());
-            }
-            return 0;
         }
-    };   
-        
-     before.sort(c);
-     after.sort(c);
-     
-     List <Method> doIt = new ArrayList<Method>();
-     for ( Method e : execute) {
-         doIt.addAll(before);
-         doIt.add(e);
-         doIt.addAll(after);
-     }
-     
-     for ( Method d: doIt) {
-         d.invoke(test);
-     }
+    }
+
+    public static List<Method> arrangeMethods(List<Method> before, List<Method> execute, List<Method> after)
+            throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+
+        List<Method> listToInvokeMethods = new ArrayList<Method>();
+
+        for (Method e : execute) {
+            listToInvokeMethods.addAll(before);
+            listToInvokeMethods.add(e);
+            listToInvokeMethods.addAll(after);
+        }
+        return listToInvokeMethods;
+    }
+
+    public static void start(JUnitTest test, List<Method> list) throws IllegalAccessException,
+            IllegalArgumentException, InvocationTargetException {
+        for (Method m : list) {
+            m.invoke(test);
+        }
+    }
+
+    private static void sortBeforeMethods(List<Method> beforeList) {
+        Comparator<Method> beforeComp = new Comparator<Method>() {
+
+            @Override
+            public int compare(Method o1, Method o2) {
+                if (o1.getAnnotation(Before.class) != null) {
+                    Before left = (Before) o1.getAnnotation(Before.class);
+                    Before right = (Before) o2.getAnnotation(Before.class);
+                    return right.priority().compareTo(left.priority());
+                }
+                return 0;
+            }
+        };
+
+        beforeList.sort(beforeComp);
+    }
+
+    public static void sortAfterMethods(List<Method> afterList) {
+        Comparator<Method> afterComp = new Comparator<Method>() {
+
+            @Override
+            public int compare(Method o1, Method o2) {
+                if (o1.getAnnotation(After.class) != null) {
+                    After left = (After) o1.getAnnotation(After.class);
+                    After right = (After) o2.getAnnotation(After.class);
+                    return right.priority().compareTo(left.priority());
+                }
+                return 0;
+            }
+        };
+        afterList.sort(afterComp);
+    }
+
+    public static void main(String[] args) throws IllegalAccessException, IllegalArgumentException,
+            InvocationTargetException {
+
+        JUnitTest test = new JUnitTest();
+
+        List<Method> before = new ArrayList<Method>();
+        List<Method> execute = new ArrayList<Method>();
+        List<Method> after = new ArrayList<Method>();
+
+        extractAnnotations(test, before, execute, after);
+
+        sortBeforeMethods(before);
+        sortAfterMethods(after);
+
+        List<Method> listToInvokeMethods = arrangeMethods(before, execute, after);
+        start(test, listToInvokeMethods);
+
     }
 }
-        
-        
-        
